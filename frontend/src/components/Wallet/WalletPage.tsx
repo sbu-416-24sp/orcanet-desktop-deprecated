79348fc47515303c75ffb6e641569fe7867e7e55
0xuana <lingxuan.fang@stonybrook.edu>
2024-03-04 16:58:24 -0500

    seperate sample data from function file

--- frontend/src/components/Wallet/WalletPage.tsx
+++ frontend/src/components/Wallet/WalletPage.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { DataTable } from "./data-table";
 import QRCode from "react-qr-code";
 import OrcaCoin from "../../svgs/orcaCoin.svg";
+import { WalletData } from "./WalletData";
 
 import {
   Card,
@@ -19,44 +20,7 @@ import Header from "../Header/Header";
 
 async function getData(): Promise<IPayment[]> {
   // Fetch data from your API here.
-  return [
-    {
-      id: "59a53ee428a643e940546c5ccfc5663e",
-      amount: -0.5523342,
-      status: "pending",
-      note: "BananaHub.mp4",
-      date: new Date("2021-10-10"),
-    },
-    {
-      id: "f0623b42ea2d521b945a80b014f5694b",
-      amount: 0.000012323,
-      status: "failed",
-      note: "Dota2_OnePunchGodModeMenu.exe",
-      date: new Date("2021-10-10"),
-    },
-    {
-      id: "061b96f36e163ef82de2feefe7d7aaba",
-      amount: -0.8311008,
-      status: "processing",
-      note: "PayPaiBalanceInjector.bin",
-      date: new Date("2021-10-10"),
-    },
-    {
-      id: "b8ae1f8845ee9cbe64174ae089973b56",
-      amount: 0.663450023,
-      status: "processing",
-      note: "",
-      date: new Date("2021-10-10"),
-    },
-    {
-      id: "bcaeff20734041e27098eb5138b3003a",
-      amount: 0.00432,
-      status: "success",
-      note: "きかんしゃトーマス.avi",
-      date: new Date("2021-10-10"),
-    },
-    // ...
-  ];
+  return WalletData;
 }
 
 export default function DemoPage() {
