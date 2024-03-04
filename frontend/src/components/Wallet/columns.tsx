9aea3b12b8b582a3ee8839515301d7aa5ebbc7f7
Qianxing Li <qianxing.li@stonybrook.edu>
2024-03-04 14:03:14 -0500

    Update wallet style, add some color to transactions

--- frontend/src/components/Wallet/columns.tsx
+++ frontend/src/components/Wallet/columns.tsx
@@ -7,6 +7,8 @@ import { ColumnDef } from "@tanstack/react-table";
 export type Payment = {
   id: string;
   amount: number;
+  note: string;
+  date: Date;
   status: "pending" | "processing" | "success" | "failed";
 };
 
@@ -14,10 +16,37 @@ export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status") as string;
+      const color =
+        status === "pending"
+          ? "text-yellow-500"
+          : status === "processing"
+          ? "text-blue-500"
+          : status === "success"
+          ? "text-green-500"
+          : "text-red-500";
+      return (
+        <div>
+          <div className={`font-medium ${color}`}>{status}</div>
+          <div className="text-gray-500">2021-10-10</div>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "id",
     header: "Transaction ID",
+    cell: ({ row }) => {
+      return (
+        <div>
+          <div className="font-medium">{row.getValue("id")}</div>
+          {row.original.note && (
+            <div className="text-gray-500">Note: {row.original.note}</div>
+          )}
+        </div>
+      );
+    },
   },
   {
     accessorKey: "amount",
@@ -25,7 +54,12 @@ export const columns: ColumnDef<Payment>[] = [
     cell: ({ row }) => {
       const amount = parseFloat(row.getValue("amount"));
       const color = amount > 0 ? "text-green-500" : "text-red-500";
-      return <div className={`text-right font-medium ${color}`}>{amount}</div>;
+      return (
+        <div>
+          <div className={`text-right font-medium ${color}`}>{amount}</div>
+          {/* <div className="text-right">Orca Coin</div> */}
+        </div>
+      );
     },
   },
 ];
