050907f6cc510c02a2c88708b9b06194ea21b2fc
Jiahui Chen <c1847568780@gmail.com>
2024-03-03 22:58:27 -0500

    Remove checkbox of Market Page, fix

--- frontend/src/components/Market/columns.tsx
+++ frontend/src/components/Market/columns.tsx
@@ -3,8 +3,6 @@ import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-import { Checkbox } from "@/components/ui/checkbox";
-
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Seed = {
