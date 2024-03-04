609ab3780476ae878b173f6bc1d4145c5cacb2ac
Jiahui Chen <c1847568780@gmail.com>
2024-03-04 13:24:08 -0500

    Add motion.div to  Market Page

--- frontend/src/components/Market/MarketPage.tsx
+++ frontend/src/components/Market/MarketPage.tsx
@@ -1,13 +1,20 @@
 import { DataTable } from "./MarketTable";
 import { columns } from "./MarketTableType";
+import { motion } from "framer-motion";
+import { routeVariants } from "../../helper/RouterAnimation";
 
 import data from "./MarketData";
 
 const MarketPage = () => {
   return (
-    <div className="p-8 h-full bg-blue-100 bg-opacity-50">
+    <motion.div
+      className="p-8 h-full bg-blue-100 bg-opacity-50"
+      initial="initial"
+      animate="final"
+      variants={routeVariants}
+    >
       <DataTable columns={columns} data={data} />
-    </div>
+    </motion.div>
   );
 };
 
