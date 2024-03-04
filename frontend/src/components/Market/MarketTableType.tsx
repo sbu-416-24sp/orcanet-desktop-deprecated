b247a2ab5272637558276cbd4f1d47ce22cf9e7e
Jiahui Chen <c1847568780@gmail.com>
2024-03-04 13:14:11 -0500

    Add action button to  Market Page

--- frontend/src/components/Market/MarketTableType.tsx
+++ frontend/src/components/Market/MarketTableType.tsx
@@ -3,6 +3,16 @@ import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type File = {
@@ -76,5 +86,33 @@ export const columns: ColumnDef<File>[] = [
       );
     },
     cell: ({ row }) => <div className="ml-6">{row.getValue("links")}</div>,
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const file = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-2 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <DotsHorizontalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(file.cid)}
+            >
+              Copy File CID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View file details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
