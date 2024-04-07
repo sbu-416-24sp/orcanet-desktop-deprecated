97e00ca4ebb86b1644e1e1bfc59d54d8b1631cd2
Zhen Wei Liao <zhenwei.liao@stonybrook.edu>
2024-04-06 23:58:00 -0400

    Refactor: Change store page to stats page

---
+++ frontend/src/stats/StatsPage.tsx
@@ -0,0 +1,11 @@
+import PageHeader from "../header/PageHeader";
+
+const StatsPage = () => {
+  return (
+    <div id="stats-page" className="size-full">
+      <PageHeader pageName="Stats" />
+    </div>
+  );
+};
+
+export default StatsPage;
