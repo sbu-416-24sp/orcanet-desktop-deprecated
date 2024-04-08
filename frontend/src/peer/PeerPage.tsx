b92e47faf3879c7af2c07da805645c3a0c0de1e9
Zhen Wei Liao <zhenwei.liao@stonybrook.edu>
2024-04-08 14:23:43 -0400

    Feature: Add peer page

---
+++ frontend/src/peer/PeerPage.tsx
@@ -0,0 +1,11 @@
+import PageHeader from "../header/PageHeader";
+
+const PeerPage = () => {
+  return (
+    <div id="peer-page" className="size-full">
+      <PageHeader pageName="Peer" />
+    </div>
+  );
+};
+
+export default PeerPage;
